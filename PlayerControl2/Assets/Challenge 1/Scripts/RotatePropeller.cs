b96a127e54d34f8ca3ba607de0cc332a1d566e80
Ben Chan <ben95chan@gmail.com>
2022-08-10 15:36:49 +0800

    V1

---
+++ PlayerControl2/Assets/Challenge 1/Scripts/RotatePropeller.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePropeller : MonoBehaviour
+{
+    public float speed = 100.0f;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(0,0,90 * Time.deltaTime * speed);
+    }
+}
