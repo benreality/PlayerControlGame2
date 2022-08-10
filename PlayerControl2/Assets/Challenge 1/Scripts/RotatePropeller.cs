2dc26a163cc9eb61bc72a9481c453a1f226e9dda
Ben Chan <ben95chan@gmail.com>
2022-08-10 16:11:04 +0800

    Update RotatePropeller.cs

--- PlayerControl2/Assets/Challenge 1/Scripts/RotatePropeller.cs
+++ PlayerControl2/Assets/Challenge 1/Scripts/RotatePropeller.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class RotatePropeller : MonoBehaviour
 {
     public float speed = 100.0f;
+   
+
   
     // Start is called before the first frame update
     void Start()
